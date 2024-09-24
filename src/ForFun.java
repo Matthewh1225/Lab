fac482944797d000763905e002d43cf2d8f7e938
Matthewh1225 <mhuckman97@yahoo.com>
2024-09-24 10:23:26 -0500

    Update ForFun.java

--- src/ForFun.java
+++ src/ForFun.java
@@ -13,7 +13,7 @@ public class ForFun {
     }
     public static void main(String[] args) {
         ForFun f = new ForFun();
-        int [] numbers = {1,2,3,4,5,6,7,8,9,10,11,13};
+        int [] numbers = {1,2,3,4,5,6,7,8,10,11,13};
         System.out.println(f.searchInsert(numbers, 5));
 
 
